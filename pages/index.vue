de2eb80bbf3aa80ac736f9a63aa3a29585ea9e6e
Lilia Ramalho Martins <lilia.martins@gmail.com>
2024-06-07 15:24:13 -0300

    tested bulma

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    <div>Page: index</div>
+  <div class="container is-fluid">
+    <div class="has-text-centered">Page: index</div>
     <div>
       <span><font-awesome-icon :icon="['fas', 'upload']" /></span>
     </div>
