73739805227db4d8b0428d6ff0dc315c0b827c98
Lilia Ramalho Martins <lilia.martins@gmail.com>
2024-06-07 15:15:51 -0300

    installed bulma

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   build: {
     transpile: ['@fortawesome/vue-fontawesome'],
   },
+  css: ['bulma/css/bulma.css', '@fortawesome/fontawesome-svg-core/styles.css'],
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
   plugins: ['~/plugins/fontawesome.ts'],
