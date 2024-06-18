268851f4afa450e1d13526f02ba6ab6286c5792c
Lilia Ramalho Martins <lilia.martins@gmail.com>
2024-06-18 10:50:06 -0300

    Better Toast styling

--- components/ToastModal.vue
+++ components/ToastModal.vue
@@ -15,16 +15,36 @@ watch(
     v-if="toastStore.isVisible"
     :class="['notification', toastStore.toastType]"
   >
-    {{ toastStore.toastMessage }}
-    <button class="delete" @click="toastStore.hideToast()"></button>
+    <div class="message-content">
+      {{ toastStore.toastMessage }}
+      <button class="delete" @click="toastStore.hideToast()"></button>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .notification {
   position: fixed;
-  top: 20px;
-  right: 20px;
+  top: 55px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: auto;
+  max-width: 400px;
+  min-width: 200px;
+  padding: 10px 20px;
   z-index: 1000;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.message-content {
+  display: flex;
+  align-items: center;
+  width: 100%;
+}
+
+.message-content .delete {
+  margin-left: 20px;
 }
 </style>
