c9f3baf76acfbb656f32b9082a0415c67ceda4d5
Lilia Ramalho Martins <lilia.martins@gmail.com>
2024-06-26 14:42:33 -0300

    list user api working

--- server/api/list-users.ts
+++ server/api/list-users.ts
@@ -1,3 +1,22 @@
-export default defineEventHandler((event) => {
-  return 'Hello list-users'
+import {
+  ListUsersCommand,
+  CognitoIdentityProviderClient,
+} from '@aws-sdk/client-cognito-identity-provider'
+import { parseAmplifyConfig } from 'aws-amplify/utils'
+import outputs from '~/amplify_outputs.json'
+
+const amplifyConfig = parseAmplifyConfig(outputs)
+
+export default defineEventHandler(async () => {
+  const listUsers = () => {
+    const client = new CognitoIdentityProviderClient({})
+
+    const command = new ListUsersCommand({
+      UserPoolId: amplifyConfig.Auth?.Cognito.userPoolId,
+    })
+
+    return client.send(command)
+  }
+
+  return listUsers()
 })
