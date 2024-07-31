a5baafea17205ccf6ce17e3828170e34eb8b894f
Lilia Ramalho Martins <lilia.martins@gmail.com>
2024-07-31 16:27:32 -0300

    refactoring new user component

--- components/Administration/NewUser.vue
+++ components/Administration/NewUser.vue
@@ -1,30 +1,5 @@
 <script setup>
-const isLoading = ref(false)
-const toastStore = useToastStore()
-const { showToast } = toastStore
-
-const save = async (email, name, nickname) => {
-  isLoading.value = true
-  const { data, error } = await useFetch('/api/administration/users', {
-    method: 'POST',
-    body: { email, name, nickname },
-  })
-  isLoading.value = false
-
-  if (error && error.value) {
-    throw createError({
-      statusCode: error.value.data?.statusCode,
-      message: error.value.data?.message,
-      fatal: true,
-    })
-  }
-
-  if (data && data.value) {
-    showToast('User created successfully!', 'is-success')
-  }
-
-  navigateTo('/administration/users')
-}
+const { save, isLoading } = useSaveNewUser()
 </script>
 
 <template>
